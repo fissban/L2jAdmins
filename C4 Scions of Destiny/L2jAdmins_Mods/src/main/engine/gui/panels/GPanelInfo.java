4cb4dd8ba59572e1b7102d17cf8c611317017aee
fissban <fissban@269d2e60-f1a3-4ecd-ab3a-fc9de945be39>
2018-12-21 16:20:17 +0000

    -- Engine - Se crea un nuevo JLabel y asi generar menos codigo en el GUI. - Se acomoda el texto mostrando informacion de las sieges y del seven signs. - Se agregan algunos comentarios al codigo como guia para otros programadores. - Se agregan algunas funciones al panel "Utils" del GUI. - Ahora se visualiza correctamente el nombre de los personajes dentro del juego.

--- C4 Scions of Destiny/L2jAdmins_Mods/src/main/engine/gui/panels/GPanelInfo.java
+++ C4 Scions of Destiny/L2jAdmins_Mods/src/main/engine/gui/panels/GPanelInfo.java
@@ -1,15 +1,13 @@
 package main.engine.gui.panels;
 
-import java.awt.Font;
-
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import main.engine.gui.model.GPanel;
 import l2j.gameserver.instancemanager.sevensigns.SevenSignsManager;
 import l2j.gameserver.instancemanager.sevensigns.enums.CabalType;
 import l2j.gameserver.instancemanager.siege.SiegeManager;
 import l2j.gameserver.util.Util;
+import main.engine.gui.model.GLabel;
+import main.engine.gui.model.GPanel;
 
 /**
  * @author fissban
@@ -17,153 +15,131 @@ import l2j.gameserver.util.Util;
 public class GPanelInfo extends JPanel
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	// Seven Signs
-	private static JLabel currentPeriod;
-	private static JLabel nextPeriod;
-	private static JLabel dawnScore;
-	private static JLabel duskScore;
+	private static GLabel currentPeriod;
+	private static GLabel nextPeriod;
+	private static GLabel dawnScore;
+	private static GLabel duskScore;
 	// Siege
-	private static JLabel gludio;
-	private static JLabel dion;
-	private static JLabel giran;
-	private static JLabel oren;
-	private static JLabel aden;
-	private static JLabel innadril;
-	private static JLabel goddard;
-	
+	private static GLabel gludio;
+	private static GLabel dion;
+	private static GLabel giran;
+	private static GLabel oren;
+	private static GLabel aden;
+	private static GLabel innadril;
+	private static GLabel goddard;
+
 	public GPanelInfo()
 	{
 		super();
 		setLayout(null);
-		
+
 		// XXX Seven Signs -----------------------------------------------------------------------------------------------
 		GPanel sevenSigns = new GPanel("Seven Signs");
 		sevenSigns.setBounds(10, 11, 754, 104);
 		add(sevenSigns);
-		
-		JLabel cp = new JLabel("Current Period:");
-		cp.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel cp = new GLabel("Current Period:");
 		cp.setBounds(10, 20, 100, 14);
 		sevenSigns.add(cp);
-		
-		currentPeriod = new JLabel();
-		currentPeriod.setFont(new Font("Tahoma", Font.BOLD, 11));
-		currentPeriod.setBounds(56, 20, 500, 14);
+
+		currentPeriod = new GLabel();
+		currentPeriod.setBounds(96, 20, 500, 14);
 		sevenSigns.add(currentPeriod);
-		
-		JLabel np = new JLabel("Next Period:");
-		np.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel np = new GLabel("Next Period:");
 		np.setBounds(10, 40, 100, 14);
 		sevenSigns.add(np);
-		
-		nextPeriod = new JLabel();
-		nextPeriod.setFont(new Font("Tahoma", Font.BOLD, 11));
-		nextPeriod.setBounds(56, 40, 500, 14);
+
+		nextPeriod = new GLabel();
+		nextPeriod.setBounds(96, 40, 500, 14);
 		sevenSigns.add(nextPeriod);
-		
-		JLabel dawn = new JLabel("Dawn Score:");
-		dawn.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel dawn = new GLabel("Dawn Score:");
 		dawn.setBounds(10, 60, 100, 14);
 		sevenSigns.add(dawn);
-		
-		dawnScore = new JLabel();
-		dawnScore.setFont(new Font("Tahoma", Font.BOLD, 11));
-		dawnScore.setBounds(56, 60, 500, 14);
+
+		dawnScore = new GLabel();
+		dawnScore.setBounds(96, 60, 500, 14);
 		sevenSigns.add(dawnScore);
-		
-		JLabel dusk = new JLabel("Dusk Score:");
-		dusk.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel dusk = new GLabel("Dusk Score:");
 		dusk.setBounds(10, 80, 100, 14);
 		sevenSigns.add(dusk);
-		
-		duskScore = new JLabel();
-		duskScore.setFont(new Font("Tahoma", Font.BOLD, 11));
-		duskScore.setBounds(56, 80, 500, 14);
+
+		duskScore = new GLabel();
+		duskScore.setBounds(96, 80, 500, 14);
 		sevenSigns.add(duskScore);
-		
+
 		// XXX Siege -----------------------------------------------------------------------------------------------------
 		GPanel siege = new GPanel("Siege");
 		siege.setBounds(10, 126, 754, 165);
 		add(siege);
-		
-		JLabel lblGludio = new JLabel("Gludio:");
-		lblGludio.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblGludio = new GLabel("Gludio:");
 		lblGludio.setBounds(10, 20, 100, 14);
 		siege.add(lblGludio);
-		
-		gludio = new JLabel();
-		gludio.setFont(new Font("Tahoma", Font.BOLD, 11));
-		gludio.setBounds(56, 20, 500, 14);
+
+		gludio = new GLabel();
+		gludio.setBounds(66, 20, 500, 14);
 		siege.add(gludio);
-		
-		JLabel lblDion = new JLabel("Dion:");
-		lblDion.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblDion = new GLabel("Dion:");
 		lblDion.setBounds(10, 40, 100, 14);
 		siege.add(lblDion);
-		
-		dion = new JLabel();
-		dion.setFont(new Font("Tahoma", Font.BOLD, 11));
-		dion.setBounds(56, 40, 500, 14);
+
+		dion = new GLabel();
+		dion.setBounds(66, 40, 500, 14);
 		siege.add(dion);
-		
-		JLabel lblGiran = new JLabel("Giran:");
-		lblGiran.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblGiran = new GLabel("Giran:");
 		lblGiran.setBounds(10, 60, 100, 14);
 		siege.add(lblGiran);
-		
-		giran = new JLabel();
-		giran.setFont(new Font("Tahoma", Font.BOLD, 11));
-		giran.setBounds(56, 60, 500, 14);
+
+		giran = new GLabel();
+		giran.setBounds(66, 60, 500, 14);
 		siege.add(giran);
-		
-		JLabel lblOren = new JLabel("Oren:");
-		lblOren.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblOren = new GLabel("Oren:");
 		lblOren.setBounds(10, 80, 100, 14);
 		siege.add(lblOren);
-		
-		oren = new JLabel();
-		oren.setFont(new Font("Tahoma", Font.BOLD, 11));
-		oren.setBounds(56, 80, 500, 14);
+
+		oren = new GLabel();
+		oren.setBounds(66, 80, 500, 14);
 		siege.add(oren);
-		
-		JLabel lblAden = new JLabel("Aden:");
-		lblAden.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblAden = new GLabel("Aden:");
 		lblAden.setBounds(10, 100, 100, 14);
 		siege.add(lblAden);
-		
-		aden = new JLabel();
-		aden.setFont(new Font("Tahoma", Font.BOLD, 11));
-		aden.setBounds(56, 100, 500, 14);
+
+		aden = new GLabel();
+		aden.setBounds(66, 100, 500, 14);
 		siege.add(aden);
-		
-		JLabel lblInnadril = new JLabel("Innadril:");
-		lblInnadril.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblInnadril = new GLabel("Innadril:");
 		lblInnadril.setBounds(10, 120, 100, 14);
 		siege.add(lblInnadril);
-		
-		innadril = new JLabel();
-		innadril.setFont(new Font("Tahoma", Font.BOLD, 11));
-		innadril.setBounds(56, 120, 500, 14);
+
+		innadril = new GLabel();
+		innadril.setBounds(66, 120, 500, 14);
 		siege.add(innadril);
-		
-		JLabel lblGoddard = new JLabel("Goddard:");
-		lblGoddard.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblGoddard.setBounds(10, 80, 100, 14);
+
+		GLabel lblGoddard = new GLabel("Goddard:");
+		lblGoddard.setBounds(10, 180, 100, 14);
 		siege.add(lblGoddard);
-		
-		goddard = new JLabel();
-		goddard.setFont(new Font("Tahoma", Font.BOLD, 11));
-		goddard.setBounds(56, 140, 500, 14);
+
+		goddard = new GLabel();
+		goddard.setBounds(66, 180, 500, 14);
 		siege.add(goddard);
-		
+
 		// XXX Manor -----------------------------------------------------------------------------------------------------
-		
+
 		GPanel manor = new GPanel("Manor");
 		manor.setBounds(10, 302, 754, 165);
 		add(manor);
 	}
-	
+
 	public void updateSevenSign()
 	{
 		currentPeriod.setText(SevenSignsManager.getInstance().getCurrentPeriod().getName());
@@ -171,13 +147,13 @@ public class GPanelInfo extends JPanel
 		dawnScore.setText(SevenSignsManager.getInstance().getCurrentScore(CabalType.DAWN) + "");
 		duskScore.setText(SevenSignsManager.getInstance().getCurrentScore(CabalType.DUSK) + "");
 	}
-	
+
 	public void updateSieges()
 	{
 		SiegeManager.getInstance().getSieges().forEach(siege ->
 		{
 			String name = siege.getCastle().getName();
-			
+
 			switch (name)
 			{
 				case "Gludio":
--- C4 Scions of Destiny/L2jAdmins_Mods/src/main/engine/gui/panels/GPanelPlayers.java
+++ C4 Scions of Destiny/L2jAdmins_Mods/src/main/engine/gui/panels/GPanelPlayers.java
@@ -3,13 +3,18 @@ package main.engine.gui.panels;
 import java.awt.Font;
 
 import javax.swing.DefaultListModel;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 
+import l2j.gameserver.model.actor.instance.L2MonsterInstance;
+import l2j.gameserver.model.actor.instance.L2RaidBossInstance;
+import main.engine.gui.model.GLabel;
 import main.engine.gui.model.GPanel;
+import main.holders.objects.CharacterHolder;
+import main.holders.objects.PlayerHolder;
+import main.util.Util;
 
 /**
  * @author fissban
@@ -17,93 +22,144 @@ import main.engine.gui.model.GPanel;
 public class GPanelPlayers extends JPanel
 {
 	private static final long serialVersionUID = 1L;
-	
+
 	// player
-	public JLabel onlines = new JLabel("0");
-	public JLabel logeds = new JLabel("0");
-	public JLabel vips = new JLabel("0");
-	public JLabel aios = new JLabel("0");
+	private GLabel onlines = new GLabel("0");
+	private GLabel logeds = new GLabel("0");
+	private GLabel vips = new GLabel("0");
+	private GLabel aios = new GLabel("0");
 	// mobs
-	public JLabel mobsDead = new JLabel("0");
-	public JLabel raidsDead = new JLabel("0");
-	
-	public DefaultListModel<String> modelPlayersOnline = new DefaultListModel<>();
-	
+	private GLabel mobsDead = new GLabel("0");
+	private GLabel raidsDead = new GLabel("0");
+
+	private DefaultListModel<String> listPlayersOnline = new DefaultListModel<>();
+	private JScrollPane spPlayer;
+
 	public GPanelPlayers()
 	{
 		super();
 		setLayout(null);
-		
+
 		GPanel panelPlayers = new GPanel("Players");
 		panelPlayers.setBounds(10, 11, 754, 52);
 		add(panelPlayers);
-		
-		JLabel lblPlayersOnline = new JLabel("Players Online:");
-		lblPlayersOnline.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblPlayersOnline = new GLabel("Players Online:");
 		lblPlayersOnline.setBounds(10, 26, 114, 14);
 		panelPlayers.add(lblPlayersOnline);
-		
-		onlines.setFont(new Font("Tahoma", Font.BOLD, 11));
+
 		onlines.setBounds(134, 26, 49, 14);
 		panelPlayers.add(onlines);
-		
-		JLabel lblPlayerLoged = new JLabel("Players Loged:");
-		lblPlayerLoged.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblPlayerLoged = new GLabel("Players Loged:");
 		lblPlayerLoged.setBounds(193, 26, 114, 14);
 		panelPlayers.add(lblPlayerLoged);
-		
-		logeds.setFont(new Font("Tahoma", Font.BOLD, 11));
+
 		logeds.setBounds(317, 26, 49, 14);
 		panelPlayers.add(logeds);
-		
-		JLabel lblVips = new JLabel("VIPs Online:");
-		lblVips.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblVips = new GLabel("VIPs Online:");
 		lblVips.setBounds(388, 26, 114, 14);
 		panelPlayers.add(lblVips);
-		
+
 		vips.setFont(new Font("Tahoma", Font.BOLD, 11));
 		vips.setBounds(512, 26, 49, 14);
 		panelPlayers.add(vips);
-		
-		JLabel lblAios = new JLabel("AIOs Online:");
-		lblAios.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblAios = new GLabel("AIOs Online:");
 		lblAios.setBounds(571, 26, 114, 14);
 		panelPlayers.add(lblAios);
-		
-		aios.setFont(new Font("Tahoma", Font.BOLD, 11));
+
 		aios.setBounds(695, 26, 49, 14);
 		panelPlayers.add(aios);
-		
+
 		GPanel panelMobs = new GPanel("Mobs");
 		panelMobs.setBounds(10, 74, 754, 52);
 		add(panelMobs);
-		
-		JLabel lblMobsDead = new JLabel("Mobs Dead:");
-		lblMobsDead.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblMobsDead = new GLabel("Mobs Dead:");
 		lblMobsDead.setBounds(10, 26, 114, 14);
 		panelMobs.add(lblMobsDead);
-		
-		mobsDead.setFont(new Font("Tahoma", Font.BOLD, 11));
+
 		mobsDead.setBounds(134, 26, 27, 14);
 		panelMobs.add(mobsDead);
-		
-		JLabel lblRaidsDead = new JLabel("Raids Dead:");
-		lblRaidsDead.setFont(new Font("Tahoma", Font.BOLD, 11));
+
+		GLabel lblRaidsDead = new GLabel("Raids Dead:");
 		lblRaidsDead.setBounds(193, 26, 114, 14);
 		panelMobs.add(lblRaidsDead);
-		
+
 		raidsDead.setFont(new Font("Tahoma", Font.BOLD, 11));
 		raidsDead.setBounds(317, 26, 27, 14);
 		panelMobs.add(raidsDead);
-		
+
 		GPanel panelPlayersOnline = new GPanel("Player Onlines");
-		panelPlayersOnline.setFont(new Font("Tahoma", Font.BOLD, 11));
 		panelPlayersOnline.setBounds(10, 137, 754, 353);
 		add(panelPlayersOnline);
-		
-		JScrollPane scrollPane = new JScrollPane(new JList<>(modelPlayersOnline));
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(10, 22, 329, 446);
-		panelPlayersOnline.add(scrollPane);
+
+		spPlayer = new JScrollPane(new JList<>(listPlayersOnline));
+		spPlayer.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		spPlayer.setBounds(10, 22, 329, 333);
+		panelPlayersOnline.add(spPlayer);
+	}
+
+	public void onKill(CharacterHolder victim)
+	{
+		if (Util.areObjectType(L2MonsterInstance.class, victim))
+		{
+			var count = Integer.parseInt(mobsDead.getText());
+			mobsDead.setText(++count + "");
+		}
+		else if (Util.areObjectType(L2RaidBossInstance.class, victim))
+		{
+			var count = Integer.parseInt(raidsDead.getText());
+			raidsDead.setText(++count + "");
+		}
+	}
+
+	public void onEnter(PlayerHolder ph)
+	{
+		listPlayersOnline.addElement(ph.getName());
+		spPlayer.updateUI();
+
+		var count = 0;
+
+		count = Integer.parseInt(logeds.getText());
+		logeds.setText(++count + "");
+
+		count = Integer.parseInt(onlines.getText());
+		onlines.setText(++count + "");
+		if (ph.isVip())
+		{
+			count = Integer.parseInt(vips.getText());
+			vips.setText(++count + "");
+		}
+
+		if (ph.isAio())
+		{
+			count = Integer.parseInt(aios.getText());
+			aios.setText(++count + "");
+		}
+	}
+
+	public void onExit(PlayerHolder ph)
+	{
+		listPlayersOnline.removeElement(ph.getName());
+
+		var count = 0;
+
+		count = Integer.parseInt(onlines.getText());
+		onlines.setText(--count + "");
+
+		if (ph.isVip())
+		{
+			count = Integer.parseInt(vips.getText());
+			vips.setText(--count + "");
+		}
+
+		if (ph.isAio())
+		{
+			count = Integer.parseInt(aios.getText());
+			aios.setText(--count + "");
+		}
 	}
 }
