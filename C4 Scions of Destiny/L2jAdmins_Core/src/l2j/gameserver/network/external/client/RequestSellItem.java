fa1bc267dc2f80dafab31d00a5ee2b28600b830d
fissban <fissban@269d2e60-f1a3-4ecd-ab3a-fc9de945be39>
2019-12-24 17:49:15 +0000

    RequestSellItem se re-escribio para tener un codigo mas claro y limpio

--- C4 Scions of Destiny/L2jAdmins_Core/src/l2j/gameserver/network/external/client/RequestSellItem.java
+++ C4 Scions of Destiny/L2jAdmins_Core/src/l2j/gameserver/network/external/client/RequestSellItem.java
@@ -1,7 +1,9 @@
 package l2j.gameserver.network.external.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import l2j.Config;
-import l2j.gameserver.illegalaction.IllegalAction;
 import l2j.gameserver.model.L2Object;
 import l2j.gameserver.model.actor.L2Npc;
 import l2j.gameserver.model.actor.instance.L2MerchantInstance;
@@ -11,7 +13,6 @@ import l2j.gameserver.network.AClientPacket;
 import l2j.gameserver.network.external.server.ActionFailed;
 import l2j.gameserver.network.external.server.ItemList;
 import l2j.gameserver.network.external.server.NpcHtmlMessage;
-import l2j.gameserver.network.external.server.SystemMessage;
 
 /**
  * This class ...
@@ -19,36 +20,44 @@ import l2j.gameserver.network.external.server.SystemMessage;
  */
 public class RequestSellItem extends AClientPacket
 {
+	public class SellItemAux
+	{
+		int objectId;
+		int count;
+		
+		public SellItemAux(int objectId, int count)
+		{
+			this.objectId = objectId;
+			this.count = count;
+		}
+	}
+	
 	private int listId;
-	private int count;
-	private int[] items; // count*3
+	private List<SellItemAux> sellItems;
 	
 	@Override
 	protected void readImpl()
 	{
 		listId = readD();
-		count = readD();
+		int count = readD();
 		if ((count <= 0) || (count > Config.MAX_ITEM_IN_PACKET))
 		{
 			count = 0;
-			items = null;
 			return;
 		}
-		items = new int[count * 3];
+		
+		sellItems = new ArrayList<>(count);
+		
 		for (int i = 0; i < count; i++)
 		{
 			int objectId = readD();
-			items[(i * 3) + 0] = objectId;
-			int itemId = readD();
-			items[(i * 3) + 1] = itemId;
-			long cnt = readD();
-			if ((cnt > Integer.MAX_VALUE) || (cnt <= 0))
+			readD();// itemId
+			int cnt = readD();
+			if (cnt <= 0)
 			{
-				count = 0;
-				items = null;
-				return;
+				continue;
 			}
-			items[(i * 3) + 2] = (int) cnt;
+			sellItems.add(new SellItemAux(objectId, cnt));
 		}
 	}
 	
@@ -67,14 +76,15 @@ public class RequestSellItem extends AClientPacket
 			return;
 		}
 		
+		// the target of the character is obtained
 		L2Object target = player.getTarget();
 		if ((target == null) || !(target.isMerchant()) || !player.isInsideRadius(target, L2Npc.INTERACTION_DISTANCE, false, false))
 		{
 			return;
 		}
 		
+		// It is expected that only an NPC of type L2 Merchant and with a list of items for sale can sell
 		L2MerchantInstance merchant = (L2MerchantInstance) target;
-		
 		if (listId > 1000000) // lease
 		{
 			if (merchant.getId() != (listId - 1000000))
@@ -84,47 +94,29 @@ public class RequestSellItem extends AClientPacket
 			}
 		}
 		
+		// total price obtained from the sale of items
 		long totalPrice = 0;
-		// Proceed the sell
-		for (int i = 0; i < count; i++)
+		// proceed the sell
+		for (SellItemAux sell : sellItems)
 		{
-			int objectId = items[(i * 3) + 0];
-			@SuppressWarnings("unused")
-			int itemId = items[(i * 3) + 1];
-			int count = items[(i * 3) + 2];
-			
-			if ((count < 0) || (count > Integer.MAX_VALUE))
-			{
-				IllegalAction.report(player, "Warning!! Character " + player.getName() + " of account " + player.getAccountName() + " tried to purchase over " + Integer.MAX_VALUE + " items at the same time.");
-				sendPacket(new SystemMessage(SystemMessage.YOU_HAVE_EXCEEDED_QUANTITY_THAT_CAN_BE_INPUTTED));
-				return;
-			}
-			
-			ItemInstance item = player.getInventory().checkItemManipulation(objectId, count, "sell");
+			ItemInstance item = player.getInventory().checkItemManipulation(sell.objectId, sell.count, "sell");
 			if ((item == null) || (!item.getItem().isSellable()))
 			{
 				continue;
 			}
 			
-			totalPrice += (item.getReferencePrice() * count) / 2;
-			if (totalPrice > Integer.MAX_VALUE)
-			{
-				IllegalAction.report(player, "Warning!! Character " + player.getName() + " of account " + player.getAccountName() + " tried to purchase over " + Integer.MAX_VALUE + " adena worth of goods.");
-				return;
-			}
-			
-			item = player.getInventory().destroyItem("Sell", objectId, count, player, null);
-			
-			/*
-			 * TODO: Disabled until Leaseholders are rewritten ;-) int price = item.getReferencePrice()*(int)count/2; L2ItemInstance li = null; L2ItemInstance la = null; if (listId > 1000000) { li = merchant.findLeaseItem(item.getItemId(),item.getEnchantLevel()); la = merchant.getLeaseAdena(); if
-			 * (li == null || la == null) continue; price = li.getPriceToBuy()*(int)count; // player sells, thus merchant buys. if (price > la.getCount()) continue; }
-			 */
-			/*
-			 * TODO: Disabled until Leaseholders are rewritten ;-) if (item != null && listId > 1000000) { li.setCount(li.getCount()+(int)count); li.updateDatabase(); la.setCount(la.getCount()-price); la.updateDatabase(); }
-			 */
+			totalPrice += (item.getReferencePrice() * sell.count) / 2;
+			// destroy item to sell
+			player.getInventory().destroyItem("Sell", sell.objectId, sell.count, player, null);
 		}
+		
+		// the greatest amount of adena that can be obtained is restricted by Integer.MAX_VALUE
+		totalPrice = Math.min(totalPrice, Integer.MAX_VALUE);
+		
+		// add adena por player
 		player.getInventory().addAdena("Sell", (int) totalPrice, merchant, false);
 		
+		// send html message
 		NpcHtmlMessage soldMsg = new NpcHtmlMessage(merchant.getObjectId());
 		if (soldMsg.setFile("data/html/merchant/" + merchant.getId() + "-sold.htm"))
 		{
@@ -132,9 +124,7 @@ public class RequestSellItem extends AClientPacket
 			player.sendPacket(soldMsg);
 		}
 		
-		// Update current load as well
-		player.updateCurLoad();
-		
+		// update item list
 		player.sendPacket(new ItemList(player, true));
 	}
 }
